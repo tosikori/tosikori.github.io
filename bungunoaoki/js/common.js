142b6ac0eae9dd5f2fe13b6a156178252b1ba45e
Toshie <test@test.test>
2015-06-16 15:51:23 +0900

    sample削除して、bungunoaoki仮アップ

---
+++ bungunoaoki/js/common.js
@@ -0,0 +1,54 @@
+// スムーズスクロール部分の記述
+$(function(){
+   // #で始まるアンカーをクリックした場合に処理
+   $('a[href^=#]').click(function() {
+      // スクロールの速度
+      var speed = 1500; // ミリ秒
+      // アンカーの値取得
+      var href= $(this).attr("href");
+      // 移動先を取得
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      // 移動先を数値で取得
+      var position = target.offset().top;
+      // スムーススクロール
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+   });
+});
+// スムーズスクロール ここまで
+
+
+// topに戻る ここから
+$(document).ready(function() {
+    var pagetop = $('.pagetop');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body, html').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+});
+// topに戻る ここまで
+
+
+// ナビ固定 ここから
+jQuery(function($) {
+
+var nav    = $('#fixedBox'),
+    offset = nav.offset();
+
+$(window).scroll(function () {
+  if($(window).scrollTop() > offset.top) {
+    nav.addClass('fixed');
+  } else {
+    nav.removeClass('fixed');
+  }
+});
+
+});
+// ナビ固定 ここまで
